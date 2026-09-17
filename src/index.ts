85733b6b91ef3b2c53e3a9ecfa70ebcaab42cbfe
quduss <qudshuaib@gmail.com>
2026-09-17 11:32:11 +0100

    Register middleware on an application level using the express app's .use method.

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,13 @@
 import express from "express";
 
 import { handlerReadiness } from "./api/readiness.js";
+import { middlewareLogResponse } from "./api/middleware.js";
 
 const app = express();
 const PORT = 8080;
 
+
+app.use(middlewareLogResponse);
 app.use("/app", express.static("./src/app"));
 
 app.get("/healthz", handlerReadiness);
