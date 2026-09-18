fff4cb16fa555922c095a0f366dacab014d0357b
quduss <qudshuaib@gmail.com>
2026-09-18 12:38:28 +0100

    Write a middleware that increments fileserverHits every time it's called.

--- src/api/middleware.ts
+++ src/api/middleware.ts
@@ -1,4 +1,5 @@
 import type { Request, Response, NextFunction } from "express";
+import { config } from "../config.js";
 
 export function middlewareLogResponse(
   req: Request,
@@ -13,5 +14,14 @@ export function middlewareLogResponse(
     }
   });
 
+  next();
+}
+
+export function middlewareMetricsInc(
+  _: Request,
+  __: Response,
+  next: NextFunction,
+) {
+  config.fileServerHits++;
   next();
 }
