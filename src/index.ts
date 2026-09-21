63c0c28d38c14efb924543b516e95f8fcabeeef3
quduss <qudshuaib@gmail.com>
2026-09-21 14:32:43 +0100

    Update the /admin/reset endpoint to only accept POST instead of GET requests

--- src/index.ts
+++ src/index.ts
@@ -16,7 +16,7 @@ app.use(middlewareLogResponse);
 app.use("/app", middlewareMetricsInc, express.static("./src/app"));
 app.get("/admin/metrics", handlerMetrics);
 app.get("/api/healthz", handlerReadiness);
-app.get("/admin/reset", handlerReset);
+app.post("/admin/reset", handlerReset);
 
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`);
