bccb57fd6c09d298c06217905aae29b01b91682f
quduss <qudshuaib@gmail.com>
2026-09-21 14:17:00 +0100

    Update the GET /api/reset to GET /admin/reset

--- src/index.ts
+++ src/index.ts
@@ -14,9 +14,9 @@ const PORT = 8080;
 
 app.use(middlewareLogResponse);
 app.use("/app", middlewareMetricsInc, express.static("./src/app"));
-app.get("/api/metrics", handlerMetrics);
+app.get("/admin/metrics", handlerMetrics);
 app.get("/api/healthz", handlerReadiness);
-app.get("/api/reset", handlerReset);
+app.get("/admin/reset", handlerReset);
 
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`);
