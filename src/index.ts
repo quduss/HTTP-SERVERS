8b435d8e084fc9824e0b15f2f6b8de321a012045
quduss <qudshuaib@gmail.com>
2026-09-22 07:01:51 +0100

    Register handlerValidateChirp in the express app

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,7 @@ import {
 } from "./api/middleware.js";
 import { handlerMetrics } from "./api/metrics.js";
 import { handlerReset } from "./api/reset.js";
+import { handlerValidateChirp } from "./api/validate_chirp.js";
 
 const app = express();
 const PORT = 8080;
@@ -17,6 +18,7 @@ app.use("/app", middlewareMetricsInc, express.static("./src/app"));
 app.get("/admin/metrics", handlerMetrics);
 app.get("/api/healthz", handlerReadiness);
 app.post("/admin/reset", handlerReset);
+app.post("/api/validate_chirp", handlerValidateChirp);
 
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`);
